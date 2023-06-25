ad9eeafb85e936038050a34898388e960b12f830
GUY7777 <guybabu777@gmail.com>
2023-06-25 17:40:05 +0300

    2

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div>
         טיימר :
-        <CountdownTimer targetTime={new Date("06/25/2023 16:57")} />
+        <CountdownTimer targetTime={new Date("06/25/2023 17:40")} />
       </div>
     </>
   );
